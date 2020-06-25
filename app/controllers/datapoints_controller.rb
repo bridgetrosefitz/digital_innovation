ca3ab6783e5345b576e734e6ed32fdfccedba5d1
bridgetrosefitz <bridgetrosefitz@gmail.com>
2020-06-25 13:53:55 +0200

    builds route, controller and show page for categories; removes show and index pages for datapoints because will only show data at indicator and category level.

--- app/controllers/datapoints_controller.rb
+++ app/controllers/datapoints_controller.rb
@@ -1,11 +1,5 @@
 class DatapointsController < ApplicationController
 
-    def index
-    end
-
-    def show
-    end
-
     def new
         @datapoint = Datapoint.new
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :datapoints, only: [:index, :show, :new, :create, :edit, :update]
+  resources :datapoints, only: [:new, :create, :edit, :update]
+
+  resources :categories, only: [:index, :show]
 
 end

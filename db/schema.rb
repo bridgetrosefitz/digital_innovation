46fb998d2b74c68b2088e29763b5009e618da692
bridgetrosefitz <bridgetrosefitz@gmail.com>
2020-06-30 14:52:26 +0200

    sets up highcharts; builds chart on Category show page; activates BCrypt gem; creates users table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_25_125005) do
+ActiveRecord::Schema.define(version: 2020_06_30_123101) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -42,6 +42,13 @@ ActiveRecord::Schema.define(version: 2020_06_25_125005) do
     t.integer "category_id"
     t.string "unit"
     t.string "scale"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "password_digest"
   end
 
 end

fc55b2a6c62a53e672e61e91db7758d0afd61b42
bridgetrosefitz <bridgetrosefitz@gmail.com>
2021-06-05 13:36:49 -0400

    Work in progress, improving decimal places and tooltip presentation

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -10,7 +10,7 @@ class CategoriesController < ApplicationController
         @category = Category.find(params[:id])
         @comments = @category.comments
         @comment = Comment.new
-        @method_to_use = params[:method_to_use] || "value"
+        @method_to_use = params[:method_to_use] || "value_to_scale"
 
         respond_to do |format|
             format.html { render 'show'}
--- app/models/datapoint.rb
+++ app/models/datapoint.rb
@@ -9,13 +9,17 @@ class Datapoint < ApplicationRecord
         value_as_float = self.value.to_f
 
         if self.indicator.scale == "billions"
-            value_as_float / 1000000000
+            value_to_display = value_as_float / 1000000000
+            value_to_display < 0.01 ? '%.3f' % value_to_display : '%.2f' % value_to_display
         elsif self.indicator.scale == "millions"
-            value_as_float / 1000000
+            value_to_display = value_as_float / 1000000
+            value_to_display < 0.01 ? '%.3f' % value_to_display : '%.2f' % value_to_display
         elsif self.indicator.scale == "thousands"
-            value_as_float / 1000
+            value_to_display = value_as_float / 1000
+            value_to_display < 0.01 ? '%.3f' % value_to_display : '%.2f' % value_to_display
         else
-            self.value
+            value_to_display = self.value
+            value_to_display < 0.01 ? '%.3f' % value_to_display : '%.2f' % value_to_display
         end
     end
 
@@ -23,12 +27,26 @@ class Datapoint < ApplicationRecord
         indicator_id = Indicator.find_by(name: "GDP").id
         normalizer_value = Datapoint.find_by(indicator_id: indicator_id, country_id: self.country_id, year: self.year).value.to_f
         self.value ? normalized_value = self.value / normalizer_value : nil
+        if normalized_value < 0.00001 
+            '%.11f' % normalized_value
+        elsif normalized_value < 0.01 
+            '%.3f' % normalized_value
+        else
+            '%.2f' % normalized_value
+        end
     end
 
     def value_population
         indicator_id = Indicator.find_by(name: "Population").id
         normalizer_value = Datapoint.find_by(indicator_id: indicator_id, country_id: self.country_id, year: self.year).value.to_f
         self.value ? normalized_value = self.value / normalizer_value : nil
+        if normalized_value < 0.01 
+            '%.3f' % normalized_value 
+        elsif normalized_value < 0.000001 
+            '%.20f' % normalized_value
+        else
+            '%.2f' % normalized_value
+        end
     end
 
 end
